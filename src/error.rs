89cc1d39ccc72713847b0c582e0718156ca02985
Deiadara <nikos@commonprefix.com>
2025-06-27 14:11:30 +0300

    feat(error): added QueuedTxMonitorError

--- src/error.rs
+++ src/error.rs
@@ -101,3 +101,15 @@ pub enum SubscriberError {
     #[error("Generic error: {0}")]
     GenericError(String),
 }
+
+#[derive(Error, Debug)]
+pub enum QueuedTxMonitorError {
+    #[error("Database error: {0}")]
+    DatabaseError(String),
+    #[error("XRPL client error: {0}")]
+    XRPLClientError(String),
+    #[error("Transaction status check failed: {0}")]
+    TransactionStatusCheckFailed(String),
+    #[error("Generic error: {0}")]
+    GenericError(String),
+}
