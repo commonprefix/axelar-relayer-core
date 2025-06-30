81c3439c7eb1ac05ed0306ac654c2e5e55413664
Deiadara <nikos@commonprefix.com>
2025-06-30 15:26:26 +0300

    fix(includer): properly handle bad task parsing

--- src/includer.rs
+++ src/includer.rs
@@ -83,8 +83,14 @@ where
         match consumer.next().await {
             Some(Ok(delivery)) => {
                 let data = delivery.data.clone();
-                let task = serde_json::from_slice::<QueueItem>(&data).unwrap();
+                let maybe_task = serde_json::from_slice::<QueueItem>(&data);
+                if maybe_task.is_err() {
+                    error!("Failed to parse task: {:?}", maybe_task.unwrap_err());
+                    delivery.ack(BasicAckOptions::default()).await.expect("ack");
+                    return;
+                }
 
+                let task = maybe_task.unwrap(); // unwrap is safe because we checked for errors above
                 let consume_res = self.consume(task).await;
                 match consume_res {
                     Ok(_) => {
