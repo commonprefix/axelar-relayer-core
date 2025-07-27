23c7cdbd9247bc33b3b6991eb61465cf27a4fef4
Dejan Cabrilo <dcabrilo@gmail.com>
2025-07-27 14:39:49 +0200

    Extract gmp_api into trait, add serial id to auditing tables

--- 0012_gmp_audit.sql
+++ 0012_gmp_audit.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS gmp_events (
-    id TEXT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
+    event_id TEXT NOT NULL,
     message_id TEXT,
     event_type TEXT NOT NULL,
     event JSONB NOT NULL,
@@ -8,8 +9,11 @@ CREATE TABLE IF NOT EXISTS gmp_events (
     updated_at TIMESTAMP WITH TIME ZONE
 );
 
+CREATE INDEX CONCURRENTLY IF NOT EXISTS gmp_events_event_id_idx ON gmp_events(event_id);
+
 CREATE TABLE IF NOT EXISTS gmp_tasks (
-    id TEXT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
+    task_id TEXT NOT NULL,
     chain TEXT NOT NULL,
     timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
     task_type TEXT NOT NULL,
@@ -19,12 +23,4 @@ CREATE TABLE IF NOT EXISTS gmp_tasks (
     updated_at TIMESTAMP WITH TIME ZONE
 );
 
--- For now, we are not going to create indices because they will slow down
--- writes and we will only ever use this table for debugging. Once indices
--- are needed, we can do something like:
--- CREATE INDEX CONCURRENTLY IF NOT EXISTS gmp_events_message_id_idx ON gmp_events(message_id);
--- CREATE INDEX CONCURRENTLY IF NOT EXISTS gmp_events_event_type_idx ON gmp_events(event_type);
--- CREATE INDEX CONCURRENTLY IF NOT EXISTS gmp_tasks_message_id_idx ON gmp_tasks(message_id);
--- CREATE INDEX CONCURRENTLY IF NOT EXISTS gmp_tasks_task_type_idx ON gmp_tasks(task_type);
-
--- Remember to add CONCURRENTLY is this is done on live db!
+CREATE INDEX CONCURRENTLY IF NOT EXISTS gmp_tasks_task_id_idx ON gmp_tasks(task_id);
