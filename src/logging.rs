07e9c402efd87703f428b7b6c423e68f28b0ede6
Dejan Cabrilo <dcabrilo@gmail.com>
2025-08-10 17:36:54 +0200

    Ideas for tracing

--- src/logging.rs
+++ src/logging.rs
@@ -1,7 +1,11 @@
+use opentelemetry_sdk::propagation::TraceContextPropagator;
 use std::any::Any;
 use crate::config::Config;
 use opentelemetry::global;
-use opentelemetry_sdk::trace::SdkTracerProvider;
+use opentelemetry::trace::TracerProvider;
+use opentelemetry_otlp::{SpanExporter, WithExportConfig};
+use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::trace::{Sampler, SdkTracerProvider};
 use sentry::integrations::opentelemetry as sentry_opentelemetry;
 use sentry::ClientInitGuard;
 use sentry_tracing::{layer as sentry_layer, EventFilter};
@@ -9,11 +13,12 @@ use tracing::level_filters::LevelFilter;
 use tracing::{info, Level};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{fmt, Layer, Registry};
+use tracing_subscriber::util::SubscriberInitExt;
 use xrpl_api::Transaction;
 use xrpl_types::AccountId;
 use ton_types::ton_types::Trace;
 
-pub fn setup_logging(config: &Config) -> ClientInitGuard {
+pub fn setup_logging(config: &Config) -> (ClientInitGuard, SdkTracerProvider) {
     let environment = std::env::var("ENVIRONMENT").unwrap_or_else(|_| "development".to_string());
 
     let guard = sentry::init((
@@ -26,32 +31,46 @@ pub fn setup_logging(config: &Config) -> ClientInitGuard {
         },
     ));
 
+    //global::set_text_map_propagator(sentry_opentelemetry::SentryPropagator::new());
+    
+    let exporter = SpanExporter::builder()
+        .with_tonic()
+        .with_endpoint("http://localhost:4317")
+        .build().unwrap();
+
+    let resource = Resource::builder()
+        .with_service_name("relayer_service")
+        .build();
+
+    let tracer_provider = SdkTracerProvider::builder()
+        .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
+            1.0,
+        ))))
+        .with_resource(resource)
+        .with_batch_exporter(exporter)
+        .build();
+
+    let tracer = tracer_provider.tracer("relayer");
+    
     let fmt_layer = fmt::layer()
         .with_target(true)
         .with_filter(LevelFilter::DEBUG);
 
     let sentry_layer = sentry_layer().event_filter(|metadata| match *metadata.level() {
-        Level::ERROR => EventFilter::Event, // Send `error` events to Sentry
-        Level::WARN => EventFilter::Event,  // Send `warn` events to Sentry
+        Level::ERROR => EventFilter::Event,
+        Level::WARN => EventFilter::Event,
         _ => EventFilter::Breadcrumb,
     });
 
-    let subscriber = Registry::default()
+    let otel_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    tracing_subscriber::registry()
         .with(fmt_layer) // Console logging
-        .with(sentry_layer); // Sentry logging
-
-    tracing::subscriber::set_global_default(subscriber)
-        .expect("Failed to set global tracing subscriber");
-
-    global::set_text_map_propagator(sentry_opentelemetry::SentryPropagator::new());
-
-    let tracer_provider = SdkTracerProvider::builder()
-        .with_span_processor(sentry_opentelemetry::SentrySpanProcessor::new())
-        .build();
-
-    global::set_tracer_provider(tracer_provider);
-
-    guard
+        .with(sentry_layer) // Sentry logging
+        .with(otel_layer) // Otel, required for Sentry tracing too
+        .init();
+    
+    (guard, tracer_provider)
 }
 
 pub fn maybe_to_string(val: &dyn Any) -> Option<String> {
